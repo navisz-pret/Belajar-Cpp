4883f1a441aa6ab26db290e7dce659be08907510
navisz-pret <navisathala37@gmail.com>
2026-06-14 08:18:23 +0700

    Remove comments and clean up output messages

--- belajar3.cpp
+++ belajar3.cpp
@@ -4,11 +4,10 @@ using namespace std;
 int main() {
     int umur = 16;
 
-    // ambacoding loh ya.
     if (umur >= 18) {
-        cout << "harus punya ktp" << endl;
+        cout << "harus punya ktp" << endl;// jika punya ktp harus 18+
     } else {
-        cout << "belum cukup umurnya mas" << endl;
+        cout << "belum cukup umurnya mas" << endl;// kalo dibawah 18 tahun berarti belum bisa
     }
     return 0;
 }
